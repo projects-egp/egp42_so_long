1e252c81ca6924bb639f29b5372c404c19a63a5c
Enrique Gil Piqueras <enrgil-p@c1r5s1.42madrid.com>
2024-11-17 00:27:43 +0100

    Just added header

--- manage_window.c
+++ manage_window.c
@@ -6,9 +6,11 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 22:53:05 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/11/02 23:03:32 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/11/17 00:26:43 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "general.h"
 
 /*HERE I PRETEND TO CREATE FUNCTIONS TO MANAGE WINDOW*/
 /*REMEMBER man, mlx_new_window functions*/
