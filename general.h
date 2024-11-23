e210b84ce5fa9660ad4f279eab6a34e5e040ad41
Enrique Gil Piqueras <enrgil-p@c3r7s1.42madrid.com>
2024-11-23 21:41:25 +0100

    General modified

--- general.h
+++ general.h
@@ -6,10 +6,26 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 21:46:08 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/11/16 23:49:53 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/11/23 21:16:28 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx_linux/mlx.h"
 #include "mlx_linux/mlx_int.h"
 #include <unistd.h>
+
+#define WIN_SIZE_X	976
+#define WIN_SIZE_Y	576
+
+/*Remember bpp == bits_per_pixel*/
+typedef	struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int	bits_per_pixel;
+	int	line_length;
+	int	endian;
+}	t_data;
+
+void	*create_image(void *mlx_ptr);
+void    my_mlx_pixel_put(t_data *data, int x, int y, int color);
