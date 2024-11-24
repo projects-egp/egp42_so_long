c3c068074498704a669421253a58ac574c67ce62
Enrique Gil Piqueras <enrgil-p@c3r7s1.42madrid.com>
2024-11-24 01:15:25 +0100

    Can draw a line with mlx_pixel_put and loop

--- init_connection.c
+++ init_connection.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 21:42:36 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/11/16 23:56:01 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2024/11/24 01:05:41 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,26 @@ int	main(void)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+//	t_data	img;
+	unsigned int	color_red;
 
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 976, 566, "Window");
+	if (mlx_ptr == NULL)
+	{
+		mlx_destroy_display(mlx_ptr);
+		free(mlx_ptr);
+	}
+	win_ptr = mlx_new_window(mlx_ptr, WIN_SIZE_X, WIN_SIZE_Y, "Window");
+//	img.img = mlx_new_image(mlx_ptr, WIN_SIZE_X, WIN_SIZE_Y);
+//	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, 
+//		&img.line_length, &img.endian);
+	color_red = 0xF9D34C;
+	for (int x = 0; x < 500; x++)
+		mlx_pixel_put(mlx_ptr, win_ptr, 0 + x, 0 + x, color_red);
+//	mlx_put_image_to_window(mlx_ptr, win_ptr, img.img, 0, 0);
 	mlx_loop(mlx_ptr);
+//	mlx_destroy_window(mlx_ptr, win_ptr);
+//	mlx_destroy_display(mlx_ptr);
+//	free(mlx_ptr);
 	return (0);
 }
