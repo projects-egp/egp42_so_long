44966828070aa91bb6803eb1dafdc366e4fc7e98
Enrique Gil Piqueras <enrgil-p@c3r3s1.42madrid.com>
2025-07-03 20:16:02 +0200

    Prints Wario, first image I used. Many things of MLX to learn yet

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/11/02 22:51:58 by enrgil-p          #+#    #+#              #
-#    Updated: 2024/11/02 23:00:36 by enrgil-p         ###   ########.fr        #
+#    Updated: 2025/04/27 15:11:43 by enrgil-p         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,7 +23,7 @@ RM = rm -f
 all: $(NAME)
 
 $(NAME): $(OBJS)
-	ar rc $(NAME) $(OBJS)
+	$(CC) $(CFLAGS) $(NAME) $(OBJS)
 #   ar: creates *.a;  r: replace if previous exist; c: creates if doesn't exist
 
 clean:
--- init_connection.c
+++ init_connection.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 21:42:36 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/11/24 01:05:41 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/07/03 20:02:51 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,45 @@ int	main(void)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+	void	*img_ptr;
+	int	x;
+	int	y;
 //	t_data	img;
 	unsigned int	color_red;
 
 	mlx_ptr = mlx_init();
 	if (mlx_ptr == NULL)
 	{
-		mlx_destroy_display(mlx_ptr);
-		free(mlx_ptr);
+		mlx_destroy_display(mlx_ptr);//Do i need this?
+		free(mlx_ptr);//Do I need this?
+		return (1);
 	}
 	win_ptr = mlx_new_window(mlx_ptr, WIN_SIZE_X, WIN_SIZE_Y, "Window");
+	if (win_ptr == NULL)
+	{
+		//Free image?
+		mlx_destroy_display(mlx_ptr);//Do i need this?
+		free(mlx_ptr);//Do I need this?
+		return (1);
+	}
+	x = 20;
+	y = 30;
+	img_ptr = mlx_xpm_file_to_image(mlx_ptr, "./assets/Wario.xpm", &x, &y);
+	while (x < 1080)
+	{
+		mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, x, y);
+		x += 80;
+	}
 //	img.img = mlx_new_image(mlx_ptr, WIN_SIZE_X, WIN_SIZE_Y);
 //	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, 
 //		&img.line_length, &img.endian);
 	color_red = 0xF9D34C;
-	for (int x = 0; x < 500; x++)
+	for (int x = 90; x < 500; x++)
 		mlx_pixel_put(mlx_ptr, win_ptr, 0 + x, 0 + x, color_red);
 //	mlx_put_image_to_window(mlx_ptr, win_ptr, img.img, 0, 0);
 	mlx_loop(mlx_ptr);
-//	mlx_destroy_window(mlx_ptr, win_ptr);
-//	mlx_destroy_display(mlx_ptr);
-//	free(mlx_ptr);
+	mlx_destroy_window(mlx_ptr, win_ptr);
+	mlx_destroy_display(mlx_ptr);
+	free(mlx_ptr);
 	return (0);
 }
