999915f65a02ee44bbe060710ae77d04291a1246
Enrique Gil Piqueras <enrgil-p@c1r10s2.42madrid.com>
2025-07-07 18:59:45 +0200

    Change protoype of map_format in general.h

--- general.h
+++ general.h
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 21:46:08 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/07/06 21:29:05 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/07/07 16:37:09 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 # include "libft/complete_libft.h"
 # include "definitions.h"
 
-int	map_format(char *map_file, int *error_flag);
+void	map_format(char *map_file);
 
 #define WIN_SIZE_X	976
 #define WIN_SIZE_Y	576
