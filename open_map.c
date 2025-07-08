f45d1bc89d1132df9c10806d51ea819b60e1dda1
Enrique Gil Piqueras <enrgil-p@c1r3s1.42madrid.com>
2025-07-08 21:20:54 +0200

    Now get_next_line() is called here

--- open_map.c
+++ open_map.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 19:35:07 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/07/08 19:46:24 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/07/08 21:12:38 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,31 @@ static void	initialize_map_data(t_map *map_data)
 	map_data->p_flag = 0;
 	map_data->c_flag = 0;
 	map_data->e_flag = 0;
-//	map_data->bottom_flag = 0;
 	map_data->error_flag = 0;
 }
 
 //Open map_pathname twice: once for check, next to flodfill and create window
 void	open_map(char *map_pathname, t_map *map_data)
 {
-	int	fd_map;
+	chars	*read_line;
+	t_list	lines;
+	int		fd_map;
 	
+	read_line = "";
+	lines = NULL;
 	initialize_map_data(map_data);
 	fd_map = open(map_pathname, O_RDONLY);
 	if (fd_map == -1)
-	{
 		map_data->error_flag = 1;
-		print_error(map_data);
+	while (read_line && !map_data->error_flag)
+	{
+		read_line = get_next_line(fd_map);
+		if (!read_line && !lines)
+			map_data->error_flag = 8;
+		else
+			check_line_by_line(read_line, map_data, &lines);
 	}
-	read_and_check_map(map_pathname, map_data, fd_map);
+	if (map_data->error_flag || !map_is_correct(map_data, &lines))
+		print_error(map_data);
+	//store in map_data->map, free nodes from lines flodfill
 }
