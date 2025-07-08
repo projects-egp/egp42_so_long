515406e3f7e2151168c778984c5213a27f483ce6
Enrique Gil Piqueras <enrgil-p@c1r3s1.42madrid.com>
2025-07-08 20:54:02 +0200

    definitions.h pass norminette

--- definitions.h
+++ definitions.h
@@ -6,26 +6,26 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 21:28:20 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/07/07 22:39:08 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/07/08 20:53:33 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef DEFINITIONS_H
 # define DEFINITIONS_H
 
 # include "general.h"
 
-#define EXPECTED_FORMAT "maps/<name-of-map-file>.ber"
+# define EXPECTED_FORMAT "maps/<name-of-map-file>.ber"
 
-typedef	struct s_map
+typedef struct s_map
 {
 	char	**map;
-	int	height;
-	int	width;
-	int	p_flag;
-	int	c_flag;
-	int	e_flag;
-	int	bottom_flag;
-	int	error_flag;//I need to think better this one
+	int		height;
+	int		width;
+	int		p_flag;
+	int		c_flag;
+	int		e_flag;
+	int		error_flag;
 }	t_map;
 
 #endif
