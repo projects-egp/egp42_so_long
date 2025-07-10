5177645a4aa287f016c798328b07777ce198a104
Enrique Gil Piqueras <enrgil-p@c1r14s2.42madrid.com>
2025-07-10 20:55:43 +0200

    Okay, I am sure here are a lot of things bad. But improves are great, hurry up, you PAINT THINGS NOW, JUST PAINT THE MAP

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 15:28:09 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/07/10 19:58:15 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/07/10 20:52:55 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,48 @@ static void	initialize_mlx_data(t_mlx *mlx_data)
 
 static void	connect_x_window(t_map *map_data, t_mlx *mlx_data)
 {
+	int	x;
+	int	y;
+	int	picture_size;
+
+	x = 0;
+	y = 0;
+	picture_size = PICTURE_SIZE;
 	mlx_data->mlx_ptr = mlx_init();
 	if (!mlx_data->mlx_ptr)
 	{
-	free(mlx_data->mlx_ptr);
+		ft_putendl_error("Error\n Init mlx failed");
+		free_strings_array(map_data->map);//Create an error_exit
+	}
+	mlx_data->win_ptr = mlx_new_window(mlx_data->mlx_ptr,
+		PICTURE_SIZE * map_data->width,
+		PICTURE_SIZE * map_data->height, NAME);
+	if (!mlx_data->win_ptr)
+	{
+		ft_putendl_error("Error\n Create window failed");
+		mlx_destroy_display(mlx_data->mlx_ptr);
+		free(mlx_data->mlx_ptr);//Do this go here
+		free_strings_array(map_data->map);//Create an error_exit
+	}
+	mlx_data->img_ptr = mlx_xpm_file_to_image(mlx_data->mlx_ptr,
+		PILOT_DOWN, &picture_size, &picture_size);
+	while (y <= map_data->height)
+	{
+		while (x <= map_data->width)
+		{
+			mlx_put_image_to_window(mlx_data->mlx_ptr,
+				mlx_data->win_ptr, mlx_data->img_ptr,
+				x * PICTURE_SIZE, y * PICTURE_SIZE);
+			x++;
+		}
+		y++;
+		x = 0;
+	}
 	mlx_loop(mlx_data->mlx_ptr);
-	return (1);//Here?
+	mlx_destroy_window(mlx_data->mlx_ptr, mlx_data->win_ptr);
+	mlx_destroy_display(mlx_data->mlx_ptr);
+				//Also, I have to free win_ptr?
+	free(mlx_data->mlx_ptr);//Do this go here
 }
 
 int	main(int arg_counter, char **arg_vector)
@@ -44,7 +80,7 @@ int	main(int arg_counter, char **arg_vector)
 			return (1);
 		}
 		initialize_mlx_data(&mlx_data);
-		connect_x_window(&map_data, &mlx_data)
+		connect_x_window(&map_data, &mlx_data);
 		free_strings_array(map_data.map);
 		return (0);
 	}
