08e021e23dc9f47d52cd9043e199cac4af71244f
Enrique Gil Piqueras <enrgil-p@c1r14s2.42madrid.com>
2025-07-10 19:28:35 +0200

    Defined texture paths

--- definitions.h
+++ definitions.h
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 21:28:20 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/07/10 14:58:50 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/07/10 19:27:32 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,12 @@
 # include "general.h"
 
 # define EXPECTED_FORMAT "maps/<name-of-map-file>.ber"
+//
+/*	* SIZES values 	*	*/
 # define MIN_MAP_SIZE 15// == (3 * 5)
 # define MAX_MAP_SIZE 3080// == (77 * 40)
+# define PICTURE_SIZE 64
+//
 /*	* ERROR_FLAG values 	*	*/
 # define WRONG_P 1
 # define WRONG_E 2
@@ -27,6 +31,17 @@
 # define WRONG_CHAR 6
 # define EMPTY_FILE 7
 # define CANNOT_WIN_GAME 8
+//
+/*	*    TEXTURES PATHS 	*	*/
+# define GRASS "./textures/grass.xpm"
+# define ITEM "./textures/item.xpm"
+# define PILOT_DOWN "./textures/pilot_down.xpm"
+# define PILOT_LEFT "./textures/pilot_left.xpm"
+# define PILOT_RIGHT "./textures/pilot_right.xpm"
+# define PILOT_UP "./textures/pilot_up.xpm"
+# define PLANE "./textures/plane.xpm"
+# define PLANE_REPAIRED "./textures/plane_repaired.xpm"
+# define TREE "./textures/tree.xpm"
 
 typedef struct s_map
 {
