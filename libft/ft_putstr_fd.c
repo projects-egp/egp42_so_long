ad4e2328cdf03521a1df81ef4dc21ea46d866326
Enrique Gil Piqueras <enrgil-p@c1r1s5.42madrid.com>
2025-07-11 22:19:21 +0200

    Now putstr writes the whole string. Tested, works

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 18:01:31 by enrgil-p          #+#    #+#             */
-/*   Updated: 2024/05/14 22:06:19 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/07/11 22:18:33 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,12 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	int	i;
-
+	int	len;
 	if (!s)
 		return ;
 	if (fd >= 0)
 	{
-		i = 0;
-		while (s[i] != '\0')
-		{
-			ft_putchar_fd(s[i], fd);
-			i++;
-		}
+		len = ft_strlen(s);
+		write(fd, s, len);
 	}
 }
-/*
-int	main(void)
-{
-	char*	text;
-	int	fildes;
-
-	text = "Hello World";
-	fildes = 1;
-	ft_putstr_fd(text, fildes);
-	return (0);
-}*/
