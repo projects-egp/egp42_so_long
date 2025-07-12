c335f177ba14965ae7d76f2744dbde8df98d2554
Enrique Gil Piqueras <enrgil-p@c1r1s5.42madrid.com>
2025-07-12 03:15:20 +0200

    Updated max size and new maps

--- definitions.h
+++ definitions.h
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 21:28:20 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/07/12 01:49:10 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/07/12 03:12:29 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 //
 /*	* SIZES values 	*	*/
 # define MIN_MAP_SIZE 15// == (3 * 5)
-# define MAX_MAP_SIZE 3080// == (77 * 40)
+# define MAX_MAP_SIZE 1920// == (77 * 40)
 # define PICTURE_SIZE 64
 //
 /*	* ERROR_FLAG values 	*	*/
